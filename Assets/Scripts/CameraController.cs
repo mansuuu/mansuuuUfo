f2d392982f248fa7cbf331d11a6e82c4604f1688
MoojinKim <mansuuu@kdml.co.kr>
2018-09-12 17:12:02 +0900

    ufo lesson complete

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        //camera transform postion - player transform postion
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-
     private Rigidbody2D rb2d;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     private void Start()
     {
+        count = 0;
         rb2d = GetComponent<Rigidbody2D>();
+        SetTextOnCountText();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -19,5 +25,23 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("needto update count");
+        if(collision.gameObject.CompareTag ("PickUp")){
+            collision.gameObject.SetActive(false);
+            count = count + 1;
+            SetTextOnCountText();
+            Debug.Log("update count");
+        }
+    }
+
+    private void SetTextOnCountText(){
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12){
+            winText.text = "You Win!";
+        }
     }
 }
